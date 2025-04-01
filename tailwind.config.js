7c674b4a97b4dee2ac78fd4e06dbb5b90a6b415a
前橋洋孝 <maehashihirotaka@maehashihirotakanoMacBook-Pro.local>
2025-04-01 11:23:25 +0900

    Tailwind設定修正・警告解消・デザイン安定化

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}", // ✅ src以下すべてのページ・コンポーネント対象
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
